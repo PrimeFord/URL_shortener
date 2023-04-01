f1f1d5f87ec4f807abfc3e53096236151a0a07e5
PrimeFord <bashfordbash@gmail.com>
2023-04-01 11:32:53 +0100

    feat: URL Shortner

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="h-[75%] w-[60%] text-center">
+        <p className="font-[400] text-[5rem]">
+          {" "}
+          Shortify My <span className="text-[#ffb83a]"> URL( )</span>
         </p>
+        <div className="flex justify-center mt-6 items-center">
+          <p className="w-[60%] h-[2rem] flex">
+            <span className="h-[2rem] w-[2rem] p-2 bg-black rounded-l-[0.45rem]">
+              <img className="w-[100%] h-[100%]" src="/image/link.png" />
+            </span>
+            <input
+              className="w-[85%] p-2 rounded-r-[0.45rem]"
+              type="text"
+              placeholder="paste link or URL here..."
+            />
+          </p>
+          <button className="w-[4.5rem] bg-black text-[#ffb83a] hover:bg-[#ffb83a] hover:text-[white] rounded-[0.45rem] p-2">
+            Shortify
+          </button>
+        </div>
+        <div className="flex items-center my-4">
+          <span>
+            ----------------------------------------------------------
+          </span>{" "}
+          <img
+            className="h-[2rem] w-[2rem]"
+            src="/image/scissors2.svg"
+            alt=""
+          />
+          <span>
+            ----------------------------------------------------------
+          </span>
+        </div>
+        <div className="flex justify-center mt-6 items-center">
+          <p className="w-[60%] h-[2rem] flex">
+            <span className="h-[2rem] w-[2rem] p-2 bg-[#ffb83a] rounded-l-[0.45rem]">
+              <img className="w-[100%] h-[100%]" src="/image/copyy.png" />
+            </span>
+            <p className="w-[85%] p-2 bg-white rounded-r-[0.45rem]"></p>
+          </p>
+          <button className="w-[4.5rem] hover:bg-black hover:text-[#ffb83a] bg-[#ffb83a] text-[white] rounded-[0.45rem] p-2">
+            Copy
+          </button>
+        </div>
+        <ul className="text-left mt-10 list-disc flex flex-col gap-3 text-[#616161]">
+          <li>
+            Paste the link to be truncated or shortened in the input field.
+          </li>
+          <li>Clik the Shortify button.</li>
+          <li>Copy the link generated in the paragraph field.</li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
