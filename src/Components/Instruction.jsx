584b4e8d3ff1f17164a6cf708432af192798918b
Muhammedfuad Bashar <115805335+PrimeFord@users.noreply.github.com>
2023-04-02 08:19:15 +0100

    Update Instruction.jsx
    feat: URL Shortener

--- src/Components/Instruction.jsx
+++ src/Components/Instruction.jsx
@@ -6,7 +6,7 @@ const Instruction = () => {
       <ul className="lg:p-0 p-3 text-left mt-10 list-disc flex flex-col gap-3 text-[#616161]">
         <li>Paste the link to be truncated or shortened in the input field.</li>
         <li>Clik the Shortify button.</li>
-        <li>Copy the link generated in the paragraph field.</li>
+        <li>Copy the link generated in the paragraph field by clicking the copy button</li>
       </ul>
     </div>
   );
